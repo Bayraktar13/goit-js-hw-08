9f8c4ea80c0c4fa6ea30ac5cdb85f5b08eab2fa5
Bayraktar13 <andriihrytsenko@gmail.com>
2023-07-04 12:52:41 +0200

    ДЗ-8

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,28 @@
+import Player from '@vimeo/player';
+import { throttle } from 'lodash';
 
+const player = new Player(document.getElementById('vimeo-player'));
+
+// Событие "timeupdate" отслеживает обновление времени воспроизведения
+player.on(
+  'timeupdate',
+  throttle(async () => {
+    const currentTime = await player.getCurrentTime();
+    localStorage.setItem('videoplayer-current-time', currentTime.toString());
+  }, 1000)
+);
+
+// При загрузке страницы проверяем, есть ли сохраненные данные и устанавливаем время воспроизведения на плеере
+window.addEventListener('DOMContentLoaded', async () => {
+  const savedTime = localStorage.getItem('videoplayer-current-time');
+  if (savedTime) {
+    const currentTime = parseFloat(savedTime);
+    if (!isNaN(currentTime)) {
+      try {
+        await player.setCurrentTime(currentTime);
+      } catch (error) {
+        console.error('Failed to set player current time:', error);
+      }
+    }
+  }
+});
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,54 @@
+import { throttle } from 'lodash.throttle';
 
+const contactFormEl = document.querySelector('.feedback-form');
+
+const userData = {};
+
+const fillFormFields = () => {
+  let dataFromLS;
+  try {
+    dataFromLS = JSON.parse(localStorage.getItem('feedback-form-state'));
+  } catch (error) {
+    console.log(error);
+  }
+  //   console.dir(contactFormEl.elements);
+  //     console.dir(dataFromLS);
+  if (dataFromLS === null) {
+    return;
+  }
+  for (const prop in dataFromLS) {
+    if (prop === 'email') {
+      contactFormEl.elements[prop].value = dataFromLS[prop];
+      //   Перебираем полученный распарсенный JSON массив из функции onFormFieldChange (данные, которые ввел пользователь. А так как elements (ключи) contactFormEl совпадают с ключами dataFromLS, присваиваем значение (value) этих ключей через [prop].value)
+    } else {
+      userData[prop] = dataFromLS[prop];
+    }
+  }
+};
+
+fillFormFields();
+
+const onFormFieldChange = throttle(event => {
+  const { target } = event;
+  const fieldName = target.name;
+  const fieldValue = target.value;
+  userData[fieldName] = fieldValue;
+  localStorage.setItem('feedback-form-state', JSON.stringify(userData));
+}, 500);
+
+const onFormSubmit = event => {
+  event.preventDefault();
+
+  const submittedData = {
+    email: contactFormEl.elements.email.value,
+    message: contactFormEl.elements.message.value,
+  };
+
+  console.log(submittedData);
+
+  localStorage.removeItem('feedback-form-state');
+  contactFormEl.reset();
+};
+
+contactFormEl.addEventListener('input', onFormFieldChange);
+contactFormEl.addEventListener('submit', onFormSubmit);
